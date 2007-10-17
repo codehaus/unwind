a806fe9ccb555316580a32ba3f55a4869ed9187b
bob <bob@e4383037-5d3c-0410-a924-8d90563d0dab>
2007-10-17 13:02:30 +0000

    much better path tracking through revision

--- bin/parent_node_creator.rb
+++ bin/parent_node_creator.rb
@@ -9,7 +9,14 @@ module Unwind
       @backing = backing
       @db.execute "CREATE TABLE 
                      paths ( 
-                       path STRING );"
+                       path STRING
+                     );"
+      @path_tracker = PathTracker.new( db )
+    end
+
+    def add_path(path, revision)
+      add_dir( path ) 
+      @path_tracker.add_path( path, revision.date )
     end
 
     def add_dir(path)
@@ -18,9 +25,19 @@ module Unwind
       @db.execute "INSERT INTO paths (path) VALUES ( '#{path}' );"
     end
 
+    def delete_path(path, revision)
+      delete_dir( path )
+      @path_tracker.delete_path( path, revision.date )
+    end
+
     def delete_dir(path)
       $logfile.puts "delete dir #{path}"
       @db.execute "DELETE FROM paths WHERE path = '#{path}' OR path LIKE '#{path}/%';"
+    end
+
+    def copy_path(path, as_of, copyfrom_path, copyfrom_as_of)
+      copy_dir( copyfrom_path, path )
+      @path_tracker.copy_path( path, as_of, copyfrom_path, copyfrom_as_of )
     end
 
     def copy_dir(from_path, to_path)
@@ -39,6 +56,17 @@ module Unwind
       ! result.empty?
     end
 
+    def has_path(path, as_of=Time.now)
+      simple = has_dir( path )
+      existance = @path_tracker.has_path( path, as_of )
+      if ( simple != existance )
+        $stderr.puts "DISAGREE: #{path} #{simple} vs #{existance}"
+      else
+        $stderr.puts "AGREE: #{path} #{simple} vs #{existance}"
+      end
+      existance
+    end
+
 
     def each
       @backing.each do |revision|
@@ -50,6 +78,21 @@ module Unwind
         #$stderr.puts "COMMIT"
         @db.execute( "COMMIT;" );
         yield revision
+        if ( false && :debug ) 
+          $stderr.puts ""
+          $stderr.puts ""
+          $stderr.puts ""
+          $stderr.puts ""
+          $stderr.puts ""
+          $stderr.puts "[Enter] to continue"
+          $logfile.puts ""
+          $logfile.puts ""
+          $logfile.puts ""
+          $logfile.puts ""
+          $logfile.puts ""
+          $logfile.flush
+          $stdin.getc
+        end
       end
     end
 
@@ -61,30 +104,38 @@ module Unwind
         skip_add = false
         add_nodes = []
         if ( node.action == 'replace' && node.kind == 'dir' )
-          delete_dir( node.path )
-          add_dir( node.path )
+          #delete_dir( node.path )
+          #add_dir( node.path )
+          delete_path( node.path, revision )
+          add_path( node.path, revision )
         elsif ( node.action == 'add' && ( node.kind == 'dir' || node.copyfrom_path != nil ) )
           $logfile.puts "#{node.path} copyfrom: #{node.copyfrom_path}"
           if ( node.copyfrom_path )
-            copy_dir( node.copyfrom_path, node.path )
+            #copy_dir( node.copyfrom_path, node.path )
+            copy_path( node.path, revision.date, node.copyfrom_path, node.copyfrom_revision.date )
           else
-            if ( has_dir( node.path ) )
+            #if ( has_dir( node.path ) )
+            if ( has_path( node.path, revision.date ) )
               skip_add = true
             else
-              add_dir( node.path )
+              #add_dir( node.path )
+              add_path( node.path, revision )
             end
           end
         elsif ( node.action == 'delete' )
-            delete_dir( node.path )
+          #delete_dir( node.path )
+          delete_path( node.path, revision )
         end
         if ( node.action == 'delete' ) 
           delete_dir( node.path )
         end
         cur = File.dirname( node.path )
-        while ( cur != '.' && ! has_dir( cur ) )
+        #while ( cur != '.' && ! has_dir( cur ) )
+        while ( cur != '.' && ! has_path( cur, revision.date ) )
           $logfile.puts "inject parent #{cur} for #{node.path}"
-          add_nodes.unshift( Node.new( revision.in_stream, { 'Node-path'=>cur, 'Node-kind'=>'dir', 'Node-action'=>'add' }, nil ) )
-          add_dir( cur )
+          add_nodes.unshift( Node.new( nil, revision.in_stream, { 'Node-path'=>cur, 'Node-kind'=>'dir', 'Node-action'=>'add' }, nil ) )
+          #add_dir( cur )
+          add_path( cur, revision )
           cur = File.dirname( node.path )
         end
         
---
+++ bin/path_tracker.rb
@@ -0,0 +1,177 @@
+
+module Unwind
+  class PathTracker
+    def initialize(db)
+      @db = db
+      @db.execute "CREATE TABLE 
+                     dir_paths ( 
+                       ts            INTEGER,
+                       path          TEXT,
+                       action        TEXT
+                     );"
+      @func = 0
+    end
+
+    def each_path()
+    end
+
+    def add_path(path, as_of)
+      $stderr.puts "add_path(#{path})"
+      db_execute( "
+        INSERT INTO dir_paths (
+          ts,
+          path,
+          action 
+        ) 
+        VALUES ( 
+          #{as_of.to_i},
+          '#{path}',
+          'add'
+        ) ;")
+      $stderr.puts "add_path(#{path}) => #{has_path(path, as_of)}"
+      #$stdin.getc
+    end
+
+    def delete_path(path, as_of=Time.now)
+      $stderr.puts "delete_path(#{path})"
+
+      db_execute( debug_path_sql( path ), true )
+      db_execute( "
+        INSERT INTO dir_paths ( 
+          ts,
+          path,
+          action 
+        ) 
+        #{generic_path_sql( "#{as_of.to_i}, path, 'del'", path, as_of, true )}
+      ;", true )
+      db_execute( debug_path_sql( path ), true )
+      $stderr.puts "delete_path(#{path}) => #{has_path(path, as_of)}"
+      #$stdin.getc
+    end
+    
+    def copy_path(path, as_of, copyfrom_path, copyfrom_as_of)
+      $stderr.puts "copy_path(#{path}, #{as_of}, #{copyfrom_path}, #{copyfrom_as_of})"
+             #'#{path}' || substr( path, #{copyfrom_path.length + 1}, length( path - #{copyfrom_path.length} ) )
+            #'rewrite/' || inner_path, 
+
+      db_execute( 
+        generic_path_sql( 
+          "#{Time.at( as_of ).to_i}, path, length(path), inner_path, length(inner_path), rewrite_path, 'add'",
+          copyfrom_path, 
+          copyfrom_as_of, 
+          true,
+          'add',
+          path
+        ), 
+      true )
+
+      db_execute( debug_path_sql( copyfrom_path ), true )
+
+      db_execute( "
+        INSERT INTO dir_paths (
+          ts,
+          path,
+          action
+        ) 
+        #{generic_path_sql( "
+              #{Time.at( as_of ).to_i}, 
+              rewrite_path,
+              'add'", 
+            copyfrom_path, 
+            copyfrom_as_of, 
+            true,
+            'add',
+            path )}
+      ;", true )
+      #db_execute( generic_path_sql( "ts, 'rewrite/' || path, 'add'", path, as_of ), true )
+      db_execute( debug_path_sql( path ), true )
+    end
+
+    def has_path(path, as_of=Time.now)
+      $stderr.puts "has_path(#{path})"
+      result = db_execute( "SELECT count(*) FROM ( #{path_exists_sql( path, as_of, false )} )", true )
+      if ( result.nil? || result.empty? || result[0][0].nil? || result[0][0] == '0' )
+        result = false
+      else
+        result = true
+      end
+      $stderr.puts "has_path(#{path}) => #{result}"
+      result 
+    end
+
+    def path_sql(path, as_of, include_subpaths)
+      generic_path_sql( 'path, ts', path, as_of, include_subpaths )
+    end
+
+    def path_exists_sql(path, as_of, include_subpaths)
+      generic_path_sql( 'path', path, as_of, include_subpaths )
+    end
+
+    def generic_path_sql(selection, path, as_of, include_subpaths, action='add', replace_prefix=nil)
+      subpath_clause = ''
+      ( subpath_clause = "OR path LIKE '#{path}/%'" ) if include_subpaths
+      action_clause = ''
+      action_clause = "WHERE dir_paths.action='#{action}'" if action
+      replace_clause = ''
+      ( replace_clause = ", '#{replace_prefix}' || substr( path, #{replace_prefix.length + 1}, length( path ) - #{path.length} ) as rewrite_path" ) if replace_prefix
+      "
+      SELECT 
+        #{selection}
+      FROM 
+        dir_paths
+      JOIN (
+        SELECT
+          max(ts) as inner_ts,
+          path    as inner_path
+          #{replace_clause}
+        FROM
+          dir_paths
+        WHERE
+          ts <= #{as_of.to_i}
+          AND ( 
+            path = '#{path}'
+            #{subpath_clause}
+          )
+        GROUP BY
+          inner_path
+       ) ON ( path = inner_path AND ts = inner_ts )
+       WHERE
+         dir_paths.action = '#{action}'
+      "
+    end
+
+    def debug_path_sql(path)
+      "
+        SELECT * FROM dir_paths WHERE path = '#{path}' OR path LIKE '#{path}/%'
+      "
+    end
+
+    def db_execute(sql, debug=false)
+      if ( debug )
+        $stderr.puts ""
+        $stderr.puts "#### DEBUG #### DEBUG #### DEBUG #### DEBUG #####"
+        $stderr.puts sql
+        $stderr.puts "#### RESULTS #### RESULTS #### RESULTS #### RESULTS #####"
+        $stderr.flush
+      end
+      results = []
+      @db.execute( sql ) do |row|
+        if ( debug ) 
+          #PP::pp row, $stderr 
+          $stderr.puts row.inspect
+        end
+        results << row
+        yield row if block_given?
+      end
+      if ( debug )
+        $stderr.puts "#### END #### END #### END #### END #####"
+        $stderr.flush
+      end
+      $stderr.puts "#{@db.changes} changes"
+      results
+    end
+
+  end
+
+
+end
